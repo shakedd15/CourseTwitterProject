1269e50db1b2e30acaa6fcb09d440c19996f0bd1
shakedd15 <Shakedd15@gmail.com>
2016-12-05 17:00:05 +0200

    parashti.

--- javaScript/TestingJava.js
+++ javaScript/TestingJava.js
@@ -1,33 +1,40 @@
+let ul, isSeccess;
+
 function assert(value, name) {
-    var placeHolder = document.getElementById("testPlace");
-    var div = document.createElement("div");
-    var li = document.createElement("li");
+    isSeccess = value;
+    ul = document.createElement("ul");
+    let placeHolder = document.getElementById("testPlace");
+    let li = document.createElement("testIfAllSuccess");
     li.style.border = "1px";
     li.style.borderColor = "black";
     li.style.borderStyle = "groove";
     li.innerHTML = name;
 
     if(value == true){
-        div.style.backgroundColor = "#8bd497";
+        li.style.backgroundColor = "#8bd497";
     }
     else {
-        div.style.backgroundColor = "#d4100c";
+        li.style.backgroundColor = "#d4100c";
     }
 
-    div.appendChild(li);
-    placeHolder.appendChild(div);
+    ul.appendChild(li);
+    placeHolder.appendChild(ul);
 }
 
 function test_group(name, test_group_function){
-    var placeHolder = document.getElementById("testPlace");
-    var div = document.createElement("div");
-    var ul = document.createElement("ul");
-    var h = document.createElement("h4");
+    let placeHolder = document.getElementById("testPlace");
+    let divTestGroup = document.createElement("div");
+    let h = document.createElement("h4");
     h.innerHTML = name;
-    placeHolder.appendChild(div);
-    div.appendChild(h);
-    div.appendChild(ul);
+    placeHolder.appendChild(divTestGroup);
+    divTestGroup.appendChild(h);
     test_group_function();
+    if(isSeccess){
+        divTestGroup.style.backgroundColor = "#8bd497";
+    }else {
+        divTestGroup.style.backgroundColor = "#d4100c";
+    }
+    divTestGroup.appendChild(ul);
 }
 
 function countingLogoImage() {
@@ -36,4 +43,51 @@ function countingLogoImage() {
 
 function countingFiveTweet() {
     return document.querySelectorAll(".row").length >= 5;
+}
+
+function findNonId() {
+    let s = $('#shaked');
+    return s.elements.length == 0;
+}
+
+function addsPapaClass() {
+    let firstCountOfShakedClass = document.querySelectorAll(".shaked").length;
+    let s = $('div');
+    s.addClass("shaked");
+    let secondCountOfShakedClass = document.querySelectorAll(".shaked").length;
+    return secondCountOfShakedClass > firstCountOfShakedClass;
+}
+
+function removeClass() {
+    let s = $('div');
+    s.addClass("shaked");
+    let firstCountOfShakedClass = document.querySelectorAll(".shaked").length;
+    s.removeClass("shaked");
+    let secondCountOfShakedClass = document.querySelectorAll(".shaked").length;
+    return firstCountOfShakedClass > secondCountOfShakedClass;
+}
+
+function changeColorWord() {
+    let s = $('a');
+    s.css("color", "green");
+    let arry = s.getAttribute("style");
+    return arry[0]["style"].color == "green";
+}
+
+function allFunctionWork(){
+    return $("b").all(function(element) {return element.innerHTML.length > 1});
+}
+
+function testIfAllSuccess() {
+    let btn = $("testIfAllSuccess").all(function (element) {
+        return element.children.length = 1;
+    });
+    return btn;
+}
+
+function testIfOneFeild() {
+    let btn = $("testIfAllSuccess").any(function (element) {
+        return element.children.length = 0;
+    });
+    return !btn;
 }
--- javaScript/ofekTwitter.js
+++ javaScript/ofekTwitter.js
@@ -3,13 +3,26 @@ window.onload = function () {
         createSingleTwitt(tweets[i].username, tweets[i].text, false);
     }
 
-    test_group('first test group', function() {
+    test_group('Selectors', function() {
         assert(countingLogoImage(), "simple successful test");
         assert(countingFiveTweet(), "counting 5 tweet-username classes under ot-body class");
+        assert(findNonId(), "not finding any non-existant ids of elements");
+    });
+
+    test_group('CSS functions', function() {
+        assert(removeClass(), "removeClass() remove papa class");
+        assert(addsPapaClass(), "addClass() adds papa class");
+        assert(changeColorWord(), "css() sets welcome-header to green");
+    });
+
+    test_group('Functional functions tests', function() {
+        assert(allFunctionWork(), "all function works with multiple functions");
+        assert(testIfAllSuccess(), "all function counts 1 child for all nav-btn class elements");
+        assert(testIfOneFeild(), "any function doesn't find a nav-btn class element with no children");
     });
 };
 
-var tweets = [
+let tweets = [
     {username: 'James Bond', text: 'Blablabla...'},
     {username: 'James Bond', text: 'im hungry'},
     {username: 'Albert Einstein', text: 'E = mc^2!'},
@@ -19,37 +32,37 @@ var tweets = [
 
 function addCommant() {
 
-    var content = document.createTextNode(document.getElementById("tweetContent").value);
-    var newUser = {username: 'Evgeny Nemzer', text: content};
+    let content = document.createTextNode(document.getElementById("tweetContent").value);
+    let newUser = {username: 'Evgeny Nemzer', text: content};
     tweets.push(newUser);
     createSingleTwitt(newUser.username, newUser.text, true)
 }
 
 //TODO: להפריד לפונקציות
 function createSingleTwitt(username, text, isThisUser){
-    var div = document.createElement("div");
+    let div = document.createElement("div");
     div.className = "row";
 
-    var firstSpan = document.createElement("span");
+    let firstSpan = document.createElement("span");
     firstSpan.className = "avatar";
 
-    var secondSpan = document.createElement("span");
+    let secondSpan = document.createElement("span");
     secondSpan.className = "text";
 
-    var img = document.createElement("img");
+    let img = document.createElement("img");
     img.src = "../images/useravatar.png";
 
-    var b = document.createElement("b");
+    let b = document.createElement("b");
     b.innerHTML = username;
 
     if(!isThisUser){
         b.style.color = '#008000';
     }
 
-    var spanForText = document.createElement("span");
+    let spanForText = document.createElement("span");
     spanForText.append(text);
 
-    var br = document.createElement("br");
+    let br = document.createElement("br");
 
     firstSpan.appendChild(img);
     secondSpan.appendChild(b);
